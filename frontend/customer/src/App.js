c43b42e93fe95b17d0b1c679890751033f2c803c
seshasaikrishnachanumolu <seshasaikrishnachanumolu@gmail.com>
2024-11-12 22:25:38 +0530

    project done

---
+++ frontend/customer/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import StudentList from './components/StudentList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Student Management System</h1>
+      <StudentList />
+    </div>
+  );
+}
+
+export default App;
